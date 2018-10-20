379aa2f17068f004ae70f55cbbb5814b525fb4da
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-10-20 23:09:39 +0900

    user_mailer.rb,change

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,13 +46,13 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-  #アカウントを有効にする
+  # アカウントを有効にする
   def activate
-    update_attribute(:activated, true)
+    update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
   end
 
-  #有効化用のメールを送信する
+  # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
