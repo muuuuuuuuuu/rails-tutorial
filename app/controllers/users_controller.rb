034c376623cf57114017310011f80e48bf2bddf2
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-10-21 00:07:02 +0900

    user._controller.rb,change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,15 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users =User.paginate(page: params[:page])
+    #@users =User.paginate(page: params[:page])
+    #上は本章のコード、下は演習のコード
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    #下は演習のコード
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
