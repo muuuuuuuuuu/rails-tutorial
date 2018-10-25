0566da276144367c6c4e58fa49c4ddcdf7a515ff
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-10-25 21:13:39 +0900

    Add user micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user, only: :destroy
+  before_action :correct_user,   only: :destroy
+
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -22,7 +23,7 @@ class MicropostsController < ApplicationController
   private
 
    def micropost_params
-     params.require(:micropost).permit(:content)
+     params.require(:micropost).permit(:content, :picture)
    end
 
    def correct_user
