f9c21f42ab19279f7b19edfb66a5bee4de579c8e
katuya nakamura <kakatutuyaya.0106@icloud.com>
2018-10-27 16:04:22 +0900

    次から難しい

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -54,4 +54,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+
+  test "should redirect following when not loggede in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect sollowers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
